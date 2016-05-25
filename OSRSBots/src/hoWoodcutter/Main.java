7ad89a5e049932cfec4cb3d44782ccf334efb687
Jesse Cason <kelpsnelp@yahoo.com>
2016-05-25 16:56:17 -0400

    Created project properly

---
+++ OSRSBots/src/hoWoodcutter/Main.java
@@ -0,0 +1,9 @@
+package hoWoodcutter;
+
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+
+@ScriptManifest(category = Category.WOODCUTTING, name = "Basic Woodcutter", author = "Opoz and Heatslinger", version = 1.0)
+public class Main {
+
+}
