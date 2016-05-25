3d5f2581f67c2fca82282d3aa9bd820cc71eb726
Nathan Liska <nbliska@w2k.vt.edu>
2016-05-25 17:03:20 -0400

    Implements basic main methods.

--- OSRSBots/src/hoWoodcutter/Main.java
+++ OSRSBots/src/hoWoodcutter/Main.java
@@ -1,9 +1,31 @@
 package hoWoodcutter;
 
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.script.AbstractScript;
 import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
 
-@ScriptManifest(category = Category.WOODCUTTING, name = "Basic Woodcutter", author = "Opoz and Heatslinger", version = 1.0)
-public class Main {
+@ScriptManifest(category = Category.WOODCUTTING, name = "hoWoodcutter", description = "Gets the wood, ya dummy.", author = "Opoz & Heatslinger", version = 1.0)
+public class Main extends AbstractScript
+{
+	
+	@Override
+	public void onStart()
+	{
+		log("Hello, you have started hoWoodcutter by Opoz and HeatSlinger, enjoy!");
+	}
+	
+	@Override
+	public int onLoop()
+	{
+		// TODO Auto-generated method stub
+		return Calculations.random(300, 500);
+	}
+	
+	@Override
+	public void onExit()
+	{
+		log("Goodbye!");
+	}
 
 }
