5e9d4374dbfd657f0b1e2c011379b7c01c75c27c
Nathan Liska <nbliska@w2k.vt.edu>
2016-05-25 17:35:03 -0400

    Fixed Areas. Created Settings class, Locations and Trees enums.

---
+++ OSRSBots/src/hoWoodcutter/util/Trees.java
@@ -0,0 +1,29 @@
+package hoWoodcutter.util;
+
+public enum Trees {
+    NORMAL("Logs", "Tree"),
+    OAK("Oak logs", "Oak tree"),
+    WILLOW("Willow logs", "Willow tree"),
+    MAPLE("Maple logs", "Maple tree"),
+    YEW("Yew logs", "Yew tree"),
+    MAGIC("Magic logs", "Magic tree");
+
+    private String logName;
+    private String treeName;
+
+    Trees(String _logName, String _treeName)
+    {
+        this.logName = _logName;
+      	this.treeName = _treeName;
+    }
+
+    public String getLogName()
+    {
+        return logName;
+    }
+
+    public String getTreeName()
+    {
+        return treeName;
+    }
+}
