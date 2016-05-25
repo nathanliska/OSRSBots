e736df55e014f0de34f341a1b37c00e674d5a2d5
Nathan Liska <nbliska@w2k.vt.edu>
2016-05-25 19:06:25 -0400

    Moved get names methods into GUI

--- OSRSBots/src/hoWoodcutter/core/Settings.java
+++ OSRSBots/src/hoWoodcutter/core/Settings.java
@@ -7,21 +7,12 @@ public class Settings {
 	//Creates references to Locations
 	private Locations locations;
 	
-	private String[] names = new String[Trees.values().length];
-	
 	public Settings() {
 		//nothing
 	}
 	
 	public Locations getLocations() {
 		return locations;
-	}
-	
-	public String[] getAllTreeNames() {
-		for (int i = 0; i < Trees.values().length; i++) {
-			names[i] = Trees.values()[i].getTreeName();
-		}
-		return names;
 	}
 	
 }
