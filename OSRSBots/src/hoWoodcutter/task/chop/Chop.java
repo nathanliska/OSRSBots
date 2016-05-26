0125cf76168e6712412b19c37578a6fa4c8bdc88
Jesse Cason <kelpsnelp@yahoo.com>
2016-05-26 11:36:04 -0400

    No longer clicks trees after every log but every 30-45 seconds

--- OSRSBots/src/hoWoodcutter/task/chop/Chop.java
+++ OSRSBots/src/hoWoodcutter/task/chop/Chop.java
@@ -38,9 +38,8 @@ public class Chop extends Node {
 		//should probably make sure you don't cut down trees that are visible but out of the bounding box, keeps happening to me
 		
 		if(tree.interact("Chop down") && script.getSettings().getLocations().getTreeArea().getArea().contains(tree.getTile())) {
-			int countLog = script.getInventory().count(script.getSettings().getLocations().getTree().getLogName());
-			AbstractScript.sleepUntil(() -> script.getInventory().count(script.getSettings().getLocations().getTree().getLogName()) > countLog, 
-					Calculations.random(13000, 15000));
+			AbstractScript.sleepUntil(() -> !tree.exists() || script.getInventory().isFull(), 
+					Calculations.random(30000, 45000));
 		} else {
 			script.getWalking().walk(script.getSettings().getLocations().getTreeArea().getArea().getCenter());
 		}
