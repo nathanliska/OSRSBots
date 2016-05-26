659ee16fa6d361142b4b74d5518b192cbfe3c268
Nathan Liska <nbliska@w2k.vt.edu>
2016-05-26 17:34:02 -0400

    Each TreeArea now contains an array of trees in that area.

--- OSRSBots/src/hoWoodcutter/util/TreeAreas.java
+++ OSRSBots/src/hoWoodcutter/util/TreeAreas.java
@@ -4,25 +4,36 @@ import org.dreambot.api.methods.map.Area;
 
 public enum TreeAreas {
 	
-	TREE_VARROCK_EAST("Varrock East", new Area()),
-	TREE_VARROCK_WEST("Varrock West", new Area()),
-	TREE_DRAYNOR_NORTH("North of Draynor", new Area(3092, 3288, 3105, 3283, 0)),
-	TREE_DRAYNOR_SE("South-East of Draynor", new Area(3118, 3233, 3096, 3215, 0));
+	TREE_VARROCK_EAST("Varrock East", new Area(), new Trees[]{Trees.NORMAL, Trees.OAK}),
+	TREE_VARROCK_WEST("Varrock West", new Area(), new Trees[]{Trees.NORMAL, Trees.OAK}),
+	TREE_DRAYNOR_NORTH("North of Draynor", new Area(3092, 3288, 3105, 3283, 0), new Trees[]{Trees.NORMAL, Trees.OAK}),
+	TREE_DRAYNOR_SE("South-East of Draynor", new Area(3118, 3233, 3096, 3215, 0), new Trees[]{Trees.NORMAL, Trees.OAK});
 	
-	private String stringArea;
+	private String areaString;
 	private Area area;
-	
-	TreeAreas(String stringArea, Area area) {
-		this.stringArea = stringArea;
+	private Trees[] areaTrees;
+
+	TreeAreas(String areaString, Area area) {
+		this.areaString = areaString;
 		this.area = area;
 	}
 	
+	TreeAreas(String areaString, Area area, Trees[] areaTrees) {
+		this.areaString = areaString;
+		this.area = area;
+		this.areaTrees = areaTrees;
+	}
+	
 	public String getStringArea() {
-		return stringArea;
+		return areaString;
 	}
 	
 	public Area getArea() {
 		return area;
+	}
+	
+	public Trees[] getAreaTrees() {
+		return areaTrees;
 	}
 
 }
