119d96fbff45f7d60434c9ef10077213495b0e67
Nathan Liska <nbliska@w2k.vt.edu>
2016-05-26 06:13:08 -0400

    Delay seems right for walking. Updated trees to remove "tree".

--- OSRSBots/src/hoWoodcutter/task/chop/ChopWalk.java
+++ OSRSBots/src/hoWoodcutter/task/chop/ChopWalk.java
@@ -33,7 +33,7 @@ public class ChopWalk extends Node {
 				public boolean verify() {
 					return script.getClient().getLocalPlayer().isMoving();
 				}
-			}, Calculations.random(5000, 7000));
+			}, Calculations.random(3000, 5000));
 		}
 		
 	}
