a1cf418a555df96478ffaf32b54fb18b3fdc7512
Nathan Liska <nbliska@w2k.vt.edu>
2016-05-26 05:50:17 -0400

    Node abstract Object moved to hoWoodcutter.task. Update can't test. GNIT

---
+++ OSRSBots/src/hoWoodcutter/task/Node.java
@@ -0,0 +1,34 @@
+package hoWoodcutter.task;
+
+import org.dreambot.api.methods.Calculations;
+
+import hoWoodcutter.hoWoodcutter;
+
+public abstract class Node {
+	
+	//Script instance to use inside of subclasses
+	public hoWoodcutter script;
+	
+	//Node constructor takes the script argument
+	public Node(hoWoodcutter script) {
+		this.script = script;
+	}
+	
+	//Delays node 100-500ms
+	public int delay() {
+		return (int)Calculations.random(100, 500);
+	}
+	
+	//Return a string
+	public String status() {
+		return "";
+	}
+	
+	//Return a priority integer (the higher the better)
+	public int priority() {
+		return 0;
+	}
+	
+	public abstract boolean validate();
+	public abstract void execute();
+}
