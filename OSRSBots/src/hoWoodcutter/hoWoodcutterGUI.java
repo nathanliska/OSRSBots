054cddbe2fdc88b4fa55b6864187c0f4b43dfdc2
Jesse Cason <kelpsnelp@yahoo.com>
2016-05-25 21:23:08 -0400

    GUI should no clonger close entire client

--- OSRSBots/src/hoWoodcutter/hoWoodcutterGUI.java
+++ OSRSBots/src/hoWoodcutter/hoWoodcutterGUI.java
@@ -76,7 +76,7 @@ public class hoWoodcutterGUI extends JFrame {
 		
 		hoWoodcutterGUI.context = main;
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 300, 250);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
