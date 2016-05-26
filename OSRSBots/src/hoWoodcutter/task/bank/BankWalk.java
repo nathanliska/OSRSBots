8a971807042e2fe6feda85ef2eded0389e66a52f
Nathan Liska <nbliska@w2k.vt.edu>
2016-05-25 20:36:52 -0400

    Updated Bank Node. Created BankWalk Node.

--- OSRSBots/src/hoWoodcutter/task/bank/BankWalk.java
+++ OSRSBots/src/hoWoodcutter/task/bank/BankWalk.java
@@ -5,25 +5,26 @@ import org.dreambot.api.script.AbstractScript;
 import hoWoodcutter.core.Node;
 import hoWoodcutter.core.Settings;
 
-public class Bank extends Node {
+public class BankWalk extends Node {
 
-	public Bank(AbstractScript _script) {
+	public BankWalk(AbstractScript _script) {
 		super(_script);
 	}
 
 	@Override
 	public String status() {
-		return "Banking.";
+		return "Walking to bank.";
 	}
 	
 	@Override
 	public boolean validate() {
-		return (script.getInventory().isFull() && Settings.location.getBankArea().getArea().contains(script.getLocalPlayer()));
+		return (script.getInventory().isFull() && !Settings.location.getBankArea().getArea().contains(script.getLocalPlayer()));
 	}
 
 	@Override
 	public void execute() {
-		// BANK ALL ITEMS.
+		// TODO Auto-generated method stub
 		
 	}
+
 }
