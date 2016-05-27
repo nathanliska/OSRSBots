94e42a5b69cd25b7a9d6dc85674eceba5c85453f
Jesse Cason <kelpsnelp@yahoo.com>
2016-05-26 23:36:33 -0400

    Comment out todos in chop

--- OSRSBots/src/hoWoodcutter/task/chop/Chop.java
+++ OSRSBots/src/hoWoodcutter/task/chop/Chop.java
@@ -32,13 +32,14 @@ public class Chop extends Node {
 
 	@Override
 	public void execute() {
+		//if(script.getSettings().getLocations().getTreeArea().getArea().contains(*Tree That Exists*)) //trying to check if the area has a tree in it that isn't cut down
 		GameObject tree = script.getGameObjects().closest(gameObject -> gameObject != null && gameObject.getName().equals(script.getSettings().getLocations().getTree().getTreeName())
 				&& gameObject.hasAction("Chop down"));
 
 		//should probably make sure you don't cut down trees that are visible but out of the bounding box, keeps happening to me
 		
 		if(tree.interact("Chop down") && script.getSettings().getLocations().getTreeArea().getArea().contains(tree.getTile())) {
-			AbstractScript.sleepUntil(() -> !tree.exists() || script.getInventory().isFull() || script.getLocalPlayer().isInCombat(), 
+			AbstractScript.sleepUntil(() -> !tree.exists() || script.getInventory().isFull() || script.getLocalPlayer().isInCombat(), //need to find a way to add onlevelup and onrandom
 					Calculations.random(30000, 45000));
 		} else {
 			script.getWalking().walk(script.getSettings().getLocations().getTreeArea().getArea().getCenter());
