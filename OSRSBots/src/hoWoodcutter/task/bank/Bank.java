50d2b0e212baeabad623c61d9ba2f5a4a96c4a62
Jesse Cason <kelpsnelp@yahoo.com>
2016-05-27 21:45:04 -0400

    Javadocs

--- OSRSBots/src/hoWoodcutter/task/bank/Bank.java
+++ OSRSBots/src/hoWoodcutter/task/bank/Bank.java
@@ -13,11 +13,22 @@ public class Bank extends Node {
 		super(script);
 	}
 
+	/**
+	 * @return banking status
+	 */
 	@Override
 	public String status() {
 		return "Banking some wood.";
 	}
 
+	/**
+	 * Checks if execute() should execute
+	 * Requires:
+	 * Inventory to have room
+	 * Be in bank area
+	 * Not be in the process of handling a level up
+	 * @return whether execute() should execute
+	 */
 	@Override
 	public boolean validate() {
 		return (script.getInventory().isFull()
@@ -25,6 +36,9 @@ public class Bank extends Node {
 				&& !script.getLevelUp());
 	}
 
+	/**
+	 * Banks all non-axe items
+	 */
 	@Override
 	public void execute() {
 		NPC banker = script.getNpcs().closest(npc -> npc != null && npc.hasAction("Bank"));
@@ -34,9 +48,9 @@ public class Bank extends Node {
 				if (script.getBank().depositAllExcept(item -> item != null && item.getName().contains("axe"))) {
 					if (AbstractScript.sleepUntil(() -> !script.getInventory().isFull(),
 							Calculations.random(5000, 8000))) {
-						if (script.getBank().close())
-							;
-						AbstractScript.sleepUntil(() -> !script.getBank().isOpen(), Calculations.random(5000, 8000));
+						if (script.getBank().close()) {
+							AbstractScript.sleepUntil(() -> !script.getBank().isOpen(), Calculations.random(5000, 8000));
+						}
 					}
 				}
 			}
