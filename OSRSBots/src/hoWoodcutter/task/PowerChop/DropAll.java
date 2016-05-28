86343dfd0d1b450ce752b968b22edd145af14713
Jesse Cason <kelpsnelp@yahoo.com>
2016-05-28 03:49:31 -0400

    Powerchopping actually added

---
+++ OSRSBots/src/hoWoodcutter/task/PowerChop/DropAll.java
@@ -0,0 +1,33 @@
+package hoWoodcutter.task.PowerChop;
+
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.script.AbstractScript;
+
+import hoWoodcutter.hoWoodcutter;
+import hoWoodcutter.task.Node;
+
+public class DropAll extends Node {
+
+	public DropAll(hoWoodcutter script) {
+		super(script);
+	}
+
+	@Override
+	public String status() {
+		return "Chopping down trees.";
+	}
+
+	@Override
+	public boolean validate() {
+		return (script.getInventory().isFull() && script.getSettings().getPowerChop() && !script.getLevelUp());
+	}
+
+	@Override
+	public void execute() {
+		if (script.getInventory().dropAllExcept(item -> item != null && item.getName().contains("axe"))) {
+			AbstractScript.sleepUntil(() -> 
+					script.getInventory().onlyContains(item -> item != null && item.getName().contains("axe")),
+					Calculations.random(20000, 30000));
+		}
+	}
+}
