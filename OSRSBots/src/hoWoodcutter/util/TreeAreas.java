42f3b6cf9371881d3320b838ee04a09c33b0a7ec
Nathan Liska <nbliska@w2k.vt.edu>
2016-05-28 02:29:03 -0400

    Created array of BankAreas inside TreeAreas.

--- OSRSBots/src/hoWoodcutter/util/TreeAreas.java
+++ OSRSBots/src/hoWoodcutter/util/TreeAreas.java
@@ -3,20 +3,20 @@ package hoWoodcutter.util;
 import org.dreambot.api.methods.map.Area;
 
 public enum TreeAreas {
-	
-	TREE_VARROCK_EAST("Varrock East", new Area(), new Trees[]{Trees.NORMAL, Trees.OAK}),
-	TREE_VARROCK_WEST("Varrock West", new Area(), new Trees[]{Trees.NORMAL, Trees.OAK}),
-	TREE_DRAYNOR_NORTH("North Draynor", new Area(3092, 3288, 3105, 3283, 0), new Trees[]{Trees.NORMAL, Trees.OAK, Trees.MAGIC}),
-	TREE_DRAYNOR_SE("South-East Draynor", new Area(3118, 3233, 3096, 3215, 0), new Trees[]{Trees.NORMAL, Trees.OAK});
+
+	TREE_DRAYNOR_NORTH("North Draynor", new Area(3092, 3288, 3105, 3283, 0), new Trees[]{Trees.NORMAL, Trees.OAK, Trees.MAGIC}, new BankAreas[]{BankAreas.BANK_DRAYNOR}),
+	TREE_DRAYNOR_SE("South-East Draynor", new Area(3118, 3233, 3096, 3215, 0), new Trees[]{Trees.NORMAL, Trees.OAK}, new BankAreas[]{BankAreas.BANK_DRAYNOR});
 	
 	private final String areaString;
 	private final Area area;
 	private final Trees[] areaTrees;
+	private final BankAreas[] bankAreas;
 	
-	TreeAreas(String areaString, Area area, Trees[] areaTrees) {
+	TreeAreas(String areaString, Area area, Trees[] areaTrees, BankAreas[] bankAreas) {
 		this.areaString = areaString;
 		this.area = area;
 		this.areaTrees = areaTrees;
+		this.bankAreas = bankAreas;
 	}
 	
 	public String getAreaName() {
@@ -29,6 +29,10 @@ public enum TreeAreas {
 	
 	public Trees[] getAreaTrees() {
 		return areaTrees;
+	}
+	
+	public BankAreas[] getBankAreas() {
+		return bankAreas;
 	}
 	
 	public boolean getAreaContains(Trees tree) {
