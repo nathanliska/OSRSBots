0ca128ef4d710932571983e700199d3aa19e9cc5
Jesse Cason <kelpsnelp@yahoo.com>
2016-05-30 16:23:56 -0400

    Should use bank booth before the banker

--- OSRSBots/src/hoWoodcutter/task/bank/Bank.java
+++ OSRSBots/src/hoWoodcutter/task/bank/Bank.java
@@ -2,6 +2,7 @@ package hoWoodcutter.task.bank;
 
 import org.dreambot.api.methods.Calculations;
 import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.wrappers.interactive.GameObject;
 import org.dreambot.api.wrappers.interactive.NPC;
 
 import hoWoodcutter.hoWoodcutter;
@@ -29,6 +30,7 @@ public class Bank extends Node {
 	 * Be in bank area
 	 * Not be in the process of handling a level up
 	 * </pre>
+	 * 
 	 * @return whether execute() should execute
 	 */
 	@Override
@@ -43,15 +45,33 @@ public class Bank extends Node {
 	 */
 	@Override
 	public void execute() {
+		GameObject booth = script.getGameObjects()
+				.closest(gameObject -> gameObject != null && gameObject.hasAction("Bank"));
 		NPC banker = script.getNpcs().closest(npc -> npc != null && npc.hasAction("Bank"));
-
-		if (banker.interact("Bank")) {
-			if (AbstractScript.sleepUntil(() -> script.getBank().isOpen(), Calculations.random(5000, 8000))) {
-				if (script.getBank().depositAllExcept(item -> item != null && item.getName().contains("axe"))) {
-					if (AbstractScript.sleepUntil(() -> !script.getInventory().isFull(),
-							Calculations.random(5000, 8000))) {
-						if (script.getBank().close()) {
-							AbstractScript.sleepUntil(() -> !script.getBank().isOpen(), Calculations.random(5000, 8000));
+		if (booth == null) {
+			if (banker.interact("Bank")) {
+				if (AbstractScript.sleepUntil(() -> script.getBank().isOpen(), Calculations.random(5000, 8000))) {
+					if (script.getBank().depositAllExcept(item -> item != null && item.getName().contains("axe"))) {
+						if (AbstractScript.sleepUntil(() -> !script.getInventory().isFull(),
+								Calculations.random(5000, 8000))) {
+							if (script.getBank().close()) {
+								AbstractScript.sleepUntil(() -> !script.getBank().isOpen(),
+										Calculations.random(5000, 8000));
+							}
+						}
+					}
+				}
+			}
+		} else {
+			if (booth.interact("Bank")) {
+				if (AbstractScript.sleepUntil(() -> script.getBank().isOpen(), Calculations.random(5000, 8000))) {
+					if (script.getBank().depositAllExcept(item -> item != null && item.getName().contains("axe"))) {
+						if (AbstractScript.sleepUntil(() -> !script.getInventory().isFull(),
+								Calculations.random(5000, 8000))) {
+							if (script.getBank().close()) {
+								AbstractScript.sleepUntil(() -> !script.getBank().isOpen(),
+										Calculations.random(5000, 8000));
+							}
 						}
 					}
 				}
